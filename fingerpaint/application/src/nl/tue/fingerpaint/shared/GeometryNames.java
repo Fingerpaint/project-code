20966976219ba10d93e16c40d533911d4a83dbdc
HugoSnel <h.d.Snel@student.tue.nl>
2013-05-28 15:28:42 +0200

    changed names of geometry constants

---
+++ fingerpaint/application/src/nl/tue/fingerpaint/shared/GeometryNames.java
@@ -0,0 +1,12 @@
+package nl.tue.fingerpaint.shared;
+
+public class GeometryNames {
+	public static final String RECT_SHORT = "RECT";
+	public static final String RECT_LONG = "Rectange 400x240";
+	public static final String SQR_SHORT = "SQR";
+	public static final String SQR_LONG = "Square";
+	public static final String CIRC_SHORT = "CIRC";
+	public static final String CIRC_LONG = "Circle";
+	public static final String JOBE_SHORT = "JOBE";
+	public static final String JOBE_LONG = "Journal Bearing";
+}
