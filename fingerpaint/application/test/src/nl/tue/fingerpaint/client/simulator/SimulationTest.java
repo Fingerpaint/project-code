2de04f4e67c4890e6f5ada3180c7a7b950b1c916
Tessa Belder <t.belder@students.tue.nl>
2013-06-12 12:10:19 +0200

    added a simulation test

---
+++ fingerpaint/application/test/src/nl/tue/fingerpaint/client/simulator/SimulationTest.java
@@ -0,0 +1,137 @@
+package nl.tue.fingerpaint.client.simulator;
+
+import nl.tue.fingerpaint.client.model.MixingProtocol;
+import nl.tue.fingerpaint.client.storage.FingerpaintJsonizer;
+import nl.tue.fingerpaint.client.storage.FingerpaintZipper;
+
+import org.junit.Test;
+
+import com.google.gwt.junit.client.GWTTestCase;
+
+/**
+ * GWT jUnit tests for the class {@link Simulation}.
+ * 
+ * @author Group Fingerpaint
+ */
+public class SimulationTest extends GWTTestCase {
+
+	private final String mixer = "mixer";
+	private final String prot = "T[0.25], B[6.25], -B[8.0], -T[5.5]";
+	private final int[] dist = new int[96000];
+	private final int runs = 10;
+	private boolean vectors = false;
+
+	private MixingProtocol protocol;
+	private String distribution;
+	private Simulation sim;
+
+	/**
+	 * Test the constructor of the Simulation class
+	 */
+	@Test
+	public void testConstructor() {
+		init();
+		
+		try {
+			sim = new Simulation(null, protocol, distribution, runs, vectors);
+			fail("An exceptionj should have been thrown");
+		} catch (NullPointerException e) {
+			assertTrue("NullpointerException was thrown, as expected", true);
+		} catch (Exception e) {
+			fail(e.toString()
+					+ "was thrown, while a NullPointerException was expected.");
+		}
+
+		try {
+			sim = new Simulation(mixer, null, distribution, runs, vectors);
+			fail("An exceptionj should have been thrown");
+		} catch (NullPointerException e) {
+			assertTrue("NullpointerException was thrown, as expected", true);
+		} catch (Exception e) {
+			fail(e.toString()
+					+ "was thrown, while a NullPointerException was expected.");
+		}
+
+		try {
+			sim = new Simulation(mixer, protocol, null, runs, vectors);
+			fail("An exceptionj should have been thrown");
+		} catch (NullPointerException e) {
+			assertTrue("NullpointerException was thrown, as expected", true);
+		} catch (Exception e) {
+			fail(e.toString()
+					+ "was thrown, while a NullPointerException was expected.");
+		}
+	}
+
+	/**
+	 * A test to check whether the {@code getMixer} function works correctly.
+	 */
+	@Test
+	public void testGetMixer() {
+		init();
+		final String result = sim.getMixer();
+		assertEquals("The mixer of the simulation", mixer, result);
+	}
+
+	/**
+	 * A test to check whether the {@code getProtocol} function works correctly.
+	 */
+	@Test
+	public void testGetProtocol() {
+		init();
+		final String result = sim.getProtocol().toString();
+		assertEquals("String representation of the protocol", prot, result);
+	}
+
+	/**
+	 * A test to check whether the {@code getConcentrationVector} function works correctly.
+	 */
+	@Test
+	public void testGetConcentrationVector() {
+		init();
+		final int[] result = FingerpaintJsonizer
+				.intArrayFromString(FingerpaintZipper.unzip(sim
+						.getConcentrationVector()));
+		for (int i = 0; i < 96000; i++) {
+		assertEquals("The distribution of the simulation at index " +i, dist[i],
+				result[i]);
+		}
+	}
+
+	/**
+	 * A test to check whether the {@code getProtocolRuns} function works correctly.
+	 */
+	@Test
+	public void testGetRuns() {
+		init();
+		final int result = sim.getProtocolRuns();
+		assertEquals("The number of runs", runs, result);
+	}
+
+	/**
+	 * A test to check whether the {@code calculatesIntermediateVectors} function works correctly.
+	 */
+	@Test
+	public void testIntermediateVectors() {
+		init();
+		final boolean result = sim.calculatesIntermediateVectors();
+		assertEquals("Calculate intermediate vectors", vectors, result);
+	}
+
+	@Override
+	public String getModuleName() {
+		return "nl.tue.fingerpaint.Fingerpaint";
+	}
+
+	// --- PRIVATE PART --------------------------------------------------
+	/**
+	 * Set up method for initialising the Simulation Object.
+	 */
+	private void init() {
+		protocol = MixingProtocol.fromString(prot);
+		distribution = FingerpaintZipper.zip(FingerpaintJsonizer
+				.toString(dist));
+		sim = new Simulation(mixer, protocol, distribution, runs, vectors);
+	}
+
+}
