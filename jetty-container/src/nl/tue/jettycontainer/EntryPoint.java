c21fb933a0ba8cfbdd1588f1d4c76c113f5decd7
Lasse Blaauwbroek <l.blaauwbroek@student.tue.nl>
2013-06-18 19:26:36 +0200

    added run option (not tested on linux)

--- jetty-container/src/nl/tue/jettycontainer/EntryPoint.java
+++ jetty-container/src/nl/tue/jettycontainer/EntryPoint.java
@@ -2,6 +2,7 @@ package nl.tue.jettycontainer;
 
 import java.io.IOException;
 import java.net.MalformedURLException;
+import java.util.Scanner;
 
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.util.resource.Resource;
@@ -9,6 +10,8 @@ import org.eclipse.jetty.util.resource.ResourceCollection;
 import org.eclipse.jetty.webapp.WebAppContext;
 
 public class EntryPoint {
+	
+	private static final String QUIT = "quit";
 	
 	public void startServer() throws MalformedURLException, IOException {
 	    Server server = new Server(80);
@@ -22,15 +25,20 @@ public class EntryPoint {
 	    context.setWar(url);
 	 
 	    server.setHandler(context);
-	    try {
-	      server.start();
-	      System.in.read();
-	      server.stop();
-	      server.join();
-	    } catch (Exception e) {
-	      e.printStackTrace();
-	      System.exit(100);
-	    }
+
+		try (Scanner scanner = new Scanner(System.in)) {
+			server.start();
+			System.out.println("\n\nType \"" + QUIT + "\" to close the server\n\n");
+			while (true) {
+				if (scanner.next().equals(QUIT)) {
+					break;
+				}
+			}
+			server.stop();
+			server.join();
+		} catch (Exception e) {
+			throw new Error(e);
+		}
 	}
  
 	public static void main(String[] args) throws Exception {
