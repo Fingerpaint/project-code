7313edfba1219b453e6369f25276d4ee1a061902
Thom Castermans <thomcastermans@users.sourceforge.net>
2013-06-18 09:26:53 +0200

    The buttons to select a drawing tool now update when the colour to draw with changes.

--- fingerpaint/src/nl/tue/fingerpaint/client/gui/buttons/CircleDrawingToolToggleButton.java
+++ fingerpaint/src/nl/tue/fingerpaint/client/gui/buttons/CircleDrawingToolToggleButton.java
@@ -4,7 +4,10 @@ import nl.tue.fingerpaint.client.Fingerpaint;
 import nl.tue.fingerpaint.client.gui.GuiState;
 import nl.tue.fingerpaint.client.model.ApplicationState;
 import nl.tue.fingerpaint.client.model.drawingtool.CircleDrawingTool;
+import nl.tue.fingerpaint.shared.utils.Colour;
 
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.dom.client.NodeList;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.ToggleButton;
@@ -33,6 +36,12 @@ public class CircleDrawingToolToggleButton extends ToggleButton implements Click
 		
 		this.fp = parent;
 		this.as = appState;
+		
+		// Initially, the circle drawing tool is not selected
+		setDown(false);
+		
+		// Initialise to default foreground colour
+		setColour(Colour.BLACK);
 
 		addClickHandler(this);
 		getElement().setId("circleDrawingTool");
@@ -51,6 +60,29 @@ public class CircleDrawingToolToggleButton extends ToggleButton implements Click
 					new CircleDrawingTool(fp.getCursorSize()));
 
 			GuiState.squareDrawingTool.setDown(false);
+			// Update faces of all buttons, because that is changed now
+			GuiState.toolMenuToggleColour.update(ToggleColourButton.UPDATE_BOTH);
+		}
+	}
+	
+	/**
+	 * Change the colour of the tool in the button.
+	 * 
+	 * @param toolColour
+	 *            The (new) colour for the tool in the button.
+	 *            When {@code null}, the colour will be reset and thus,
+	 *            the default colour will be used.
+	 */
+	public void setColour(Colour toolColour) {
+		NodeList<Element> children = getElement().getElementsByTagName("div");
+		for (int i = 0; i < children.getLength(); i++) {
+			if (children.getItem(i).getClassName().equals("html-face")) {
+				if (toolColour != null) {
+					children.getItem(i).getStyle().setBackgroundColor(toolColour.toHexString());
+				} else {
+					children.getItem(i).getStyle().clearBackgroundColor();
+				}
+			}
 		}
 	}
 }
